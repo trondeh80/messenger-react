687c6dd8cf8931313bd69335e686aa441878769a
trondeh80 <trond.erling@gmail.com>
2021-06-18 10:55:16 +0200

    Readability in custom hook

--- src/util/use-chats.js
+++ src/util/use-chats.js
@@ -1,37 +1,33 @@
-import React, { useContext, useEffect, useState } from 'react';
+import {useContext, useEffect, useState} from 'react';
 import ENUMS from './enums';
 import MessengerContext from '../context/messenger-context';
-import { fetchChatForUser } from '../services/messenger-service';
+import {fetchChatForUser} from '../services/messenger-service';
 
 export default function useChats(userId) {
-    const [error, setError] = useState('');
-    const [isLoading, setIsLoading] = useState(true);
-    const {
-        dispatch
-    } = useContext(MessengerContext);
+  const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+  const {dispatch} = useContext(MessengerContext);
 
-    useEffect(fetchData, [userId]);
+  useEffect(fetchData, [userId, dispatch]);
 
-    // custom hook returns value
-    return { error, isLoading };
+  // custom hook returns only errors/isloading state.
+  return {error, isLoading};
 
-    function fetchData() {
-        if (!userId) {
-            return;
-        }
-
-        fetchChatForUser(userId)
-            .then((messages) => {
-                dispatch({
-                    type: ENUMS.MESSENGER.ACTIONS.MESSAGES_LOAD,
-                    data: {
-                        messages: [...messages]
-                    }
-                });
-            })
-            .catch(setError)
-            .finally(() => {
-                setIsLoading(false);
-            });
+  function fetchData() {
+    if (!userId) {
+      return;
     }
+
+    fetchChatForUser(userId)
+      .then((messages) =>
+        dispatch({
+          type: ENUMS.MESSENGER.ACTIONS.MESSAGES_LOAD,
+          data: {
+            messages: [...messages],
+          },
+        })
+      )
+      .catch(setError)
+      .finally(() => setIsLoading(false));
+  }
 }
